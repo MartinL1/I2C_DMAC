2129e208a7ce19ec20d201c83ceb7edfcc8230d5
MartinL1 <31612615+MartinL1@users.noreply.github.com>
2018-02-20 08:29:03 +0000

    Update DMAC.h

--- DMAC/DMAC.h
+++ DMAC/DMAC.h
@@ -4,7 +4,9 @@
 	Copyright (C) Martin Lindupp 2018
 	
 	Initial release -- V1.0.0 
-  The MIT License (MIT)
+  
+	The MIT License (MIT)
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
